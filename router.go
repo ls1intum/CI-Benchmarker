a79721548f2d18040c0ae9adf64b8431af3a95b2
Matthias Linhuber <matthias@linhuber.org>
2024-08-30 19:08:43 +0200

    Read uuid from request

--- router.go
+++ router.go
@@ -11,6 +11,16 @@ import (
 
 var persister Persister
 
+type ResultMetadata struct {
+	JobName                  string `json:"jobName" env:"JOB_NAME"`
+	UUID                     string `json:"uuid" env:"UUID"`
+	AssignmentRepoBranchName string `json:"assignmentRepoBranchName" env:"ASSIGNMENT_REPO_BRANCH_NAME" envDefault:"main"`
+	IsBuildSuccessful        bool   `json:"isBuildSuccessful" env:"IS_BUILD_SUCCESSFUL"`
+	AssignmentRepoCommitHash string `json:"assignmentRepoCommitHash" env:"ASSIGNMENT_REPO_COMMIT_HASH"`
+	TestsRepoCommitHash      string `json:"testsRepoCommitHash" env:"TESTS_REPO_COMMIT_HASH"`
+	BuildCompletionTime      string `json:"buildCompletionTime" env:"BUILD_COMPLETION_TIME"`
+}
+
 func startRouter() *gin.Engine {
 	// Create a db instance - ugly hack - should be refactored
 	slog.Debug("Creating DB persister")
@@ -54,7 +64,21 @@ func benchmarkHades(c *gin.Context) {
 func handleResult(c *gin.Context) {
 	slog.Debug("Received result", slog.Any("result", c.Request.Body))
 
-	uuid := uuid.New() //TODO: Get the UUID from the request
+	// Get the UUID and the time from the request
+	var resultMetadata ResultMetadata
+	if err := c.ShouldBindJSON(&resultMetadata); err != nil {
+		slog.Error("Failed to bind JSON", slog.Any("error", err))
+		c.JSON(400, gin.H{"error": "Failed to bind JSON"})
+		return
+	}
+
+	uuid, err := uuid.Parse(resultMetadata.UUID)
+	if err != nil {
+		slog.Error("Failed to parse UUID", slog.Any("error", err))
+		c.JSON(400, gin.H{"error": "Failed to parse UUID"})
+		return
+	}
+
 	time := time.Now()
 
 	persister.StoreResult(uuid, time)
