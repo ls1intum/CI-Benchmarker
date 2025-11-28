09d5d0f7d3b8b6b9698fbde5bf10c14546d973ec
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-11-28 15:48:26 +0100

    inject UUID into job payload metadata for JenkinsExecutor

--- executor/jenkins-executor.go
+++ executor/jenkins-executor.go
@@ -44,72 +44,80 @@ func (e *JenkinsExecutor) Name() string {
 func (e *JenkinsExecutor) Execute(jobPayload payload.RESTPayload) (uuid.UUID, error) {
 	slog.Debug("Executing JenkinsExecutor")
 
+	// Create UUID for this benchmark job
+	jobUUID := uuid.New()
+
+	// Inject UUID into payload metadata
+	if jobPayload.Metadata == nil {
+		jobPayload.Metadata = make(map[string]string)
+	}
+	jobPayload.Metadata["UUID"] = jobUUID.String()
+
+	slog.Info("Assigned UUID to jobPayload", slog.String("uuid", jobUUID.String()))
+
+	// Validate Jenkins config
 	if e.JenkinsURL == "" || e.User == "" || e.APIToken == "" || e.JobPath == "" {
 		slog.Debug("JenkinsExecutor not configured properly")
-		return uuid.UUID{}, errors.New("JenkinsExecutor not configured: need JenkinsURL, User, APIToken, JobPath")
+		return jobUUID, errors.New("JenkinsExecutor not configured: need JenkinsURL, User, APIToken, JobPath")
 	}
 
+	// Get Jenkins crumb
 	crumbField, crumbValue, err := e.getCrumb()
 	if err != nil {
 		slog.Debug("Error while getting Jenkins crumb")
-		return uuid.UUID{}, err
+		return jobUUID, err
 	}
 
 	var endpoint string
 	var req *http.Request
 
+	// Build request
 	if e.UseParameters {
 		params, err := e.payloadToParams(jobPayload)
+		// NOTE: params now contain the UUID inside HADES_PAYLOAD_JSON
 		if err != nil {
 			slog.Debug("Error while serializing payload")
-			return uuid.UUID{}, err
+			return jobUUID, err
 		}
-		endpoint = e.JenkinsURL + "/" + strings.TrimLeft(e.JobPath, "/") + "/buildWithParameters"
 
+		endpoint = e.JenkinsURL + "/" + strings.TrimLeft(e.JobPath, "/") + "/buildWithParameters"
 		req, err = http.NewRequest(http.MethodPost, endpoint, strings.NewReader(params.Encode()))
 		if err != nil {
 			slog.Debug("Error while creating POST request to Jenkins (parameters)")
-			return uuid.UUID{}, err
+			return jobUUID, err
 		}
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
 	} else {
 		endpoint = e.JenkinsURL + "/" + strings.TrimLeft(e.JobPath, "/") + "/build"
-
-		var err error
 		req, err = http.NewRequest(http.MethodPost, endpoint, nil)
 		if err != nil {
 			slog.Debug("Error while creating POST request to Jenkins (no parameters)")
-			return uuid.UUID{}, err
+			return jobUUID, err
 		}
 	}
 
+	// Set auth & crumb
 	req.SetBasicAuth(e.User, e.APIToken)
 	if crumbField != "" && crumbValue != "" {
 		req.Header.Set(crumbField, crumbValue)
 	}
 
+	// Send request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		slog.Debug("Error while sending POST request to Jenkins")
-		return uuid.UUID{}, err
+		return jobUUID, err
 	}
 	defer resp.Body.Close()
 
+	// Validate Jenkins response
 	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusAccepted {
 		slog.Debug("JenkinsExecutor returned non-201/202 status code", slog.Int("status", resp.StatusCode))
-		return uuid.UUID{}, errors.New("JenkinsExecutor returned non-201/202 status code")
+		return jobUUID, errors.New("JenkinsExecutor returned non-201/202 status code")
 	}
 
-	loc := strings.TrimSpace(resp.Header.Get("Location"))
-	if loc == "" {
-		slog.Debug("JenkinsExecutor missing Location header")
-		return uuid.UUID{}, errors.New("JenkinsExecutor response missing Location header (queue item url)")
-	}
-
-	id := uuid.NewSHA1(uuid.NameSpaceURL, []byte(strings.TrimRight(loc, "/")))
-	slog.Info("JenkinsExecutor queued successfully", slog.String("queue_url", loc), slog.Any("jobID", id))
-
-	return id, nil
+	return jobUUID, nil
 }
 
 func (e *JenkinsExecutor) getCrumb() (field string, value string, err error) {
@@ -140,20 +148,11 @@ func (e *JenkinsExecutor) getCrumb() (field string, value string, err error) {
 }
 
 func (e *JenkinsExecutor) payloadToParams(p payload.RESTPayload) (url.Values, error) {
-	if p.Metadata == nil {
-		p.Metadata = make(map[string]string)
-	}
-
-	jenkinsID := uuid.New().String()
-	p.Metadata["jenkinsId"] = jenkinsID
-
+	values := url.Values{}
 	b, err := json.Marshal(p)
 	if err != nil {
 		return nil, err
 	}
-
-	values := url.Values{}
 	values.Set("HADES_PAYLOAD_JSON", string(b))
-
 	return values, nil
 }
